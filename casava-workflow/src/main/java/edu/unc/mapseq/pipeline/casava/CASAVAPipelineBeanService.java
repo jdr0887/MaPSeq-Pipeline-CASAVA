de02f6582ae83213f9ca083406186d87104e8913
Jason Reilly <jdr0887@renci.org>
2013-05-09 15:46:16 -0400

    adding sitename

--- casava-workflow/src/main/java/edu/unc/mapseq/pipeline/casava/CASAVAPipelineBeanService.java
+++ casava-workflow/src/main/java/edu/unc/mapseq/pipeline/casava/CASAVAPipelineBeanService.java
@@ -4,8 +4,18 @@ import edu.unc.mapseq.pipeline.AbstractPipelineBeanService;
 
 public class CASAVAPipelineBeanService extends AbstractPipelineBeanService {
 
+    private String siteName;
+
     public CASAVAPipelineBeanService() {
         super();
+    }
+
+    public String getSiteName() {
+        return siteName;
+    }
+
+    public void setSiteName(String siteName) {
+        this.siteName = siteName;
     }
 
 }
