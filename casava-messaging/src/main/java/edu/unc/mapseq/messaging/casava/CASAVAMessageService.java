f496811396598f85a3446ebd89f0ae3d406cec8e
Jason Reilly <jdr0887@renci.org>
2014-06-06 16:37:27 -0400

    initial commit

---
+++ casava-messaging/src/main/java/edu/unc/mapseq/messaging/casava/CASAVAMessageService.java
@@ -0,0 +1,75 @@
+package edu.unc.mapseq.messaging.casava;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CASAVAMessageService {
+
+    private final Logger logger = LoggerFactory.getLogger(CASAVAMessageService.class);
+
+    private ConnectionFactory connectionFactory;
+
+    private CASAVAMessageListener messageListener;
+
+    private String destinationName;
+
+    private Connection connection;
+
+    private Session session;
+
+    public CASAVAMessageService() {
+        super();
+    }
+
+    public void start() throws Exception {
+        logger.info("ENTERING start()");
+        this.connection = connectionFactory.createConnection();
+        this.session = this.connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        Destination destination = this.session.createQueue(this.destinationName);
+        MessageConsumer consumer = this.session.createConsumer(destination);
+        consumer.setMessageListener(getMessageListener());
+        this.connection.start();
+    }
+
+    public void stop() throws Exception {
+        logger.info("ENTERING stop()");
+        if (this.session != null) {
+            this.session.close();
+        }
+        if (this.connection != null) {
+            this.connection.stop();
+            this.connection.close();
+        }
+    }
+
+    public ConnectionFactory getConnectionFactory() {
+        return connectionFactory;
+    }
+
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    public CASAVAMessageListener getMessageListener() {
+        return messageListener;
+    }
+
+    public void setMessageListener(CASAVAMessageListener messageListener) {
+        this.messageListener = messageListener;
+    }
+
+    public String getDestinationName() {
+        return destinationName;
+    }
+
+    public void setDestinationName(String destinationName) {
+        this.destinationName = destinationName;
+    }
+
+}
